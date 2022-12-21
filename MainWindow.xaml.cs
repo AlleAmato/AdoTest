bb400dbb415a7efa5da3d3910963ccfcc1e5be55
AlleAmato <alleamato13@gmail.com>
2022-12-21 12:26:02 +0100

    Aggiungere i file di progetto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using AdoTest.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdoTest
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new MainWindowViewModel();
+            DataContext = vm;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Click();//chiamo il metodo dal view model faccio solo da passacarte nel code behind
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using AdoTest.Models;
+
+namespace AdoTest.ViewModels
+{
+
+    internal class MainWindowViewModel
+    {
+        internal void Click()
+        {
+    string connectionString =   @"Server=DESKTOP-24NCMVV\SQLEXPRESS; Database=Test; 
+                                        Integrated Security=True; TrustServerCertificate=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    //non usare mai concatenazione di stringhe per prendere parametri
+                    connection.Open();
+                    /*SqlCommand command = new SqlCommand();
+                    command.CommandText = "insert into Configurations([key], [value]) values('Font', 'Comic Sans')";
+                    command.Connection = connection;
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "select count(*) as Conteggio from Configurations";
+                    int righe = (int)command.ExecuteScalar();
+                    MessageBox.Show(righe.ToString());*/
+                    List<Configuration> configurations = new List<Configuration>();//creo la lista di oggetti configuration
+                    SqlCommand command = new SqlCommand("Select * from Configurations", connection);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())//mi permette di scorrere tutto
+                        {
+                            //prendo le righe e creo gli oggetti dal database
+                            Configuration c = new Configuration
+                            {
+                                Id = (int)reader["Id"],
+                                Key = (string)reader["Key"],
+                                Value = (string)reader["Value"]
+                            };
+                            configurations.Add(c);//aggiungo la riga alla lista
+                        }
+                            
+                    }
+                }
+                catch(Exception){
+                    throw;
+                }
+            }
+        }
+    }
+}
